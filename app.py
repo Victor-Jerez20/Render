e409107cc98c51075de38712d43d4ee6aed91976
Victor-Jerez20 <expedientes@uregional.edu.gt>
2025-06-13 10:32:47 -0600

    Modificación de app.py
    Se modifica el app.py para que el endpoint de flask acepte el parametro en la url

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ import requests
 
 app = Flask(__name__)
 
-GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzwKjD78MUIWiNbHuE95a4q1tFRRpE-v25RX-WnUQlK3xMVgY-BCky-SORIPaUeVyr7/exec"
+GOOGLE_SCRIPT_URL = "https://script.google.com/macros/library/d/1tEExU9z6cHr_dumFU4NhK9lh6tPQyt5IfZmBQIBlyEleDSemlTGd5jlp/1"
 
 @app.route("/datos", methods=["GET"])
 def obtener_datos():
+    sheet = request.args.get("sheet")
+    
+    if not sheet:
+        return jsonify({"error": "Falta el parámetro 'sheet'"}), 400
+    
     try:
-        respuesta = requests.get(GOOGLE_SCRIPT_URL)
+        # Enviar el parámetro 'sheet' al Apps Script
+        respuesta = requests.get(GOOGLE_SCRIPT_URL, params={"sheet": sheet})
         if respuesta.status_code == 200:
             return jsonify({"valor": respuesta.text})
         else:
